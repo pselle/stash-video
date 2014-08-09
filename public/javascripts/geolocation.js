9ba23c58b989ac38fcc925153edbd6cfa9a02e83
Victor Lourng <hello@victorlourng.com>
2014-08-09 12:55:12 -0400

    Geolocation

---
+++ public/javascripts/geolocation.js
@@ -0,0 +1,45 @@
+function success(position) {
+  var s = document.querySelector('.location');
+  
+  if (s.className == 'location') {
+    // not sure why we're hitting this twice in FF, I think it's to do with a cached result coming back    
+    return;
+  }
+  
+  s.innerHTML = "found you!";
+  s.className = 'location';
+  
+  console.log(position.coords.latitude);p
+  console.log(position.coords.longitude);p
+  console.log(position.coords.accuracy);p
+}
+
+function error(msg) {
+  var s = $('.location');
+  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+  s.className = 'fail';
+  
+  // console.log(arguments);
+}
+
+if (navigator.geolocation) {
+  
+} else {
+  error('not supported');
+}
+
+// SMooth scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
