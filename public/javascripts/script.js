7b99ebddf72f34f1f140e94f4fda85db2b20ba17
pselle <pamela.selle@gmail.com>
2014-08-09 09:57:34 -0400

    Add client side JS file

---
+++ public/javascripts/script.js
@@ -0,0 +1,9 @@
+// Click record a video button launches camera (native) (back button rerecords video)
+// User records video
+// What's the problem? multiple choice
+// If "Other" 
+// Show the "more information" text box
+// Submit button?
+// Submit form data
+// Success
+console.log('client side loaded');
